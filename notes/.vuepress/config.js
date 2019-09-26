26c898d6d6636dc658f4709c40addf95f3ea7502
Khank <khanh@construct.sg>
2019-09-26 09:49:33 +0700

    [master] Add favicon

--- notes/.vuepress/config.js
+++ notes/.vuepress/config.js
@@ -5,6 +5,17 @@ module.exports = {
   description: 'Wikipedia của tui',
   base: '/docs/',
   dest: '../docs',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "../public/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "../public/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "../public/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "../public/favicons/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "../public/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+    ['link', { rel: "shortcut icon", href: "../public/favicons/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    ['meta', { name: "msapplication-config", content: "../public/favicons/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   themeConfig: {
     nav: [
       { text: 'Home', link: 'https://nguyenngockhank.github.io' },
@@ -73,5 +84,13 @@ module.exports = {
         '@': path.resolve()
       }
     }
-  }
+  },
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-148742356-1' // UA-00000000-0
+      }
+    ]
+  ]
 }
