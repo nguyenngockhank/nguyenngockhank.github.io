3a4cb5c2b17885613632083ac3b71a5919165d89
nguyenkhank <apple@Khank.local>
2020-11-29 14:18:29 +0700

    add nosqldistilled

--- notes/.vuepress/sidebar/main.js
+++ notes/.vuepress/sidebar/main.js
@@ -66,6 +66,7 @@ module.exports = [
       title: "Database",
       children: [
         '/db/architect',
+        '/db/use-cases',
         '/db/optimize',
         '/db/mysql_snippets',
         '/db/postgre',
