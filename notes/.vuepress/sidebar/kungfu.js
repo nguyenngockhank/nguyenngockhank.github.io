26ab78aca63eb360433bf2916ffd92a6f162b1bf
nguyenkhank <apple@Khank.local>
2021-01-01 17:03:06 +0700

    add clean cooooode

--- notes/.vuepress/sidebar/kungfu.js
+++ notes/.vuepress/sidebar/kungfu.js
@@ -1,15 +1,27 @@
 
 module.exports = [
     {
-        title: '97 Things Every Programmer Should Know',
-        children: [
-            ...Array(98).fill(0).map((v, index) => `97things4dev/${index}`)
-        ]
-    },
-    {
         title: 'The Art of Readable Code',
         children: [
             ...Array(15).fill(0).map((v, index) => `artofcode/c${index + 1}`)
+        ]
+    },
+    {
+        title: 'Clean Code',
+        children: [
+            ...Array(17).fill(0).map((v, index) => `cleancode/c${index + 1}`)
+        ]
+    },
+    {
+        title: 'A Philosophy of Software Design',
+        children: [
+            ...Array(21).fill(0).map((v, index) => `philosophy/c${index + 1}`)
+        ]
+    },
+    {
+        title: '97 Things Every Programmer Should Know',
+        children: [
+            ...Array(98).fill(0).map((v, index) => `97things4dev/${index}`)
         ]
     },
     {
@@ -17,12 +29,6 @@ module.exports = [
         children: [
             'nosql-distilled/00',
             ...Array(16).fill(0).map((v, index) => `nosql-distilled/c${index}`)
-        ]
-    },
-    {
-        title: 'A Philosophy of Software Design',
-        children: [
-            ...Array(21).fill(0).map((v, index) => `philosophy/c${index + 1}`)
         ]
     },
     {
