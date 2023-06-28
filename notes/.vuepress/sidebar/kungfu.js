ba476cbaed3921301517cf1601d6a2c62cf5d400
Nguyen Khanh <nguyenkhanh@Nguyens-Mac-mini.local>
2023-06-28 18:11:07 +0700

    feat: add design s3

--- notes/.vuepress/sidebar/kungfu.js
+++ notes/.vuepress/sidebar/kungfu.js
@@ -11,6 +11,7 @@ module.exports = [
                 children: [
                     'case-study/dynamo/',
                     'case-study/bigtable/',
+                    'case-study/s3/',
                 ]
             },
             {
